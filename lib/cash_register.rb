525f16e26185df22a3bb99bec24110d0d8aa2134
Shahwar Spall <shevy.spall@gmail.com>
2019-08-13 05:15:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@
 class CashRegister
   attr_accessor :discount, :total,  :last
   
-  def initialize(discount = 0)
+  def initialize(discount = nil)
     @items = []
     @total = 0 
     @discount = discount
