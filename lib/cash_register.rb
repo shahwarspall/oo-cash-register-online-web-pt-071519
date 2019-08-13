1e4943fafb1dce4c419762f2509d953f6ed83855
Shahwar Spall <shevy.spall@gmail.com>
2019-08-13 04:01:25 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :discount, :total
+  
+  def initialize(discount = 0)
+    @total = []
+    @total = 0 
+    @discount = discount
+  
+  end 
+  
+  def add_item(title,price,increase = 1)
+    self.total 
+    binding.pry
+  end
+ 
+ end
